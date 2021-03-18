340656ac57f3d90bb79007c2b7626ca686616398
james <jmbegley@gmx.com>
2021-03-17 22:19:47 -0400

    removed c_getslope; renamed c_getslope_ptr to c_getslope

--- main.c
+++ main.c
@@ -2,14 +2,14 @@
 #include "xypair.h"
 
 extern double jmbcftest(double);
-extern double c_getslope_ptr(struct xypair_t*, struct xypair_t*);
+extern double c_getslope(struct xypair_t*, struct xypair_t*);
 
 int main(int argc, char** argv) {
 	double testee = jmbcftest(800.0);
 	printf("jmbcftest(800.0) returns: %f\n", testee);
 	struct xypair_t p1 = {0, 0};
 	struct xypair_t p2 = {400, 300};
-	double slope = c_getslope_ptr(&p1, &p2);
-	printf("c_getslope_ptr(p1, p2) returns: %f\n", slope);
+	double slope = c_getslope(&p1, &p2);
+	printf("c_getslope(p1, p2) returns: %f\n", slope);
 	return 0;
 }
--- triangulator.f90
+++ triangulator.f90
@@ -9,21 +9,14 @@ module triangulator
                         type(xypair_t), value :: p1, p2
                         getslope=((p2%y-p1%y)/(p2%x-p1%x))
                 end function getslope
-                function c_getslope(p1, p2) bind(C)
+                function c_getslope(p1, p2) bind (C)
                         implicit none
                         real(c_double) c_getslope
-                        type(xypair_t), value :: p1, p2
-                        !c_getslope=((p2%y-p1%y)/(p2%x-p1%x))
-                        c_getslope=getslope(p1, p2)
-                end function c_getslope
-                function c_getslope_ptr(p1, p2) bind (C)
-                        implicit none
-                        real(c_double) c_getslope_ptr
                         type(c_ptr), value :: p1, p2
                         type(xypair_t), pointer :: f1, f2
                         call c_f_pointer(p1, f1)
                         call c_f_pointer(p2, f2)
-                        !c_getslope_ptr=((f2%y-f1%y)/(f2%x-f1%x))
-                        c_getslope_ptr=getslope(f1, f2)
-                end function c_getslope_ptr
+                        !c_getslope=((f2%y-f1%y)/(f2%x-f1%x))
+                        c_getslope=getslope(f1, f2)
+                end function c_getslope
 end module triangulator
